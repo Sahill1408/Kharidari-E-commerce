503d24add1d2ad18d52620f174e38ad7022208f9
Sahil Dhanavade <72405721+Sahill1408@users.noreply.github.com>
2025-06-29 17:08:53 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,5 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   base: '/',
-  // ... rest of your config
+  plugins: [react()],
 })
 
